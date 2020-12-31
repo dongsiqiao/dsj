995c9b016e256df5da261c53c0a39c857e194d19
dongsiqiao <1247143911@qq.com>
2020-12-31 11:35:58 +0800

    08_获取并渲染用户信息

---
+++ assets/js/index.js
@@ -0,0 +1,30 @@
+$(function () {
+  getUserInfo()
+})
+function getUserInfo() {
+  $.ajax({
+    method: 'GET',
+    url: '/my/userinfo',
+    headers: {
+      Authorization: localStorage.getItem('token') || ''
+    },
+    success(res) {
+      if (res.status !== 0) {
+        return layui.layui.msg('获取信息失败')
+      }
+      renderAvatar(res.data)
+    }
+  })
+}
+function renderAvatar(user) {
+  let name = user.nickname || user.username
+  $('#welcome').html('欢迎&nbsp;' + name)
+  if (user.user_pic !== null) {
+    $('.layui-nav-img').attr('src', user.user_pic).show()
+    $('.text-avatar').hide()
+  } else {
+    $('.layui-nav-img').hide()
+    let first = name[0].toUpperCase()
+    $('.text-avatar').html(first).show()
+  }
+}
